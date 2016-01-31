fbb04c4cc7b3a13eae12cad3282541e3fde0fa6a
Marc Bogaerts <marcbogaerts@telenet.be>
2016-01-31 16:00:41 +0100

    Sleuth correlation ID

--- src/main/java/demo02/GreetingsController.java
+++ src/main/java/demo02/GreetingsController.java
@@ -5,6 +5,8 @@ import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,11 @@ public class GreetingsController {
     	TimeUnit.MILLISECONDS.sleep(RandomUtils.nextLong(50L, 500L));
     	
         return quotes.chooseOne();
+    }
+
+    @Bean
+    public AlwaysSampler defaultSampler() {
+      return new AlwaysSampler();
     }
 
     public static void main(String[] args) throws Exception {
