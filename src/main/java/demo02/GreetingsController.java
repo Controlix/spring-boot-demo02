a89cbf9e4556f56dd707e2c81e58c75a6a242fb8
Marc Bogaerts <marcbogaerts@telenet.be>
2016-03-11 18:35:39 +0100

    Spectator metrics

--- src/main/java/demo02/GreetingsController.java
+++ src/main/java/demo02/GreetingsController.java
@@ -1,6 +1,8 @@
 package demo02;
 import java.util.concurrent.TimeUnit;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.lang3.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +12,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.spectator.api.DistributionSummary;
+import com.netflix.spectator.api.Registry;
+import com.netflix.spectator.api.Timer;
+
 @SpringBootApplication
 @RestController
 public class GreetingsController {
@@ -17,12 +23,31 @@ public class GreetingsController {
 	@Autowired
 	private QuoteRepo quotes;
 	
-    @RequestMapping("/greetings")
-    public String index() throws InterruptedException {
-    	TimeUnit.MILLISECONDS.sleep(RandomUtils.nextLong(50L, 500L));
+	@Autowired
+	private Registry registry;
+
+	private DistributionSummary distributionSummary;
+
+	private Timer timer;
+	
+	@PostConstruct
+    public void init() {
+		distributionSummary = registry.distributionSummary("messageSize");
+		timer = registry.timer("messageTime");
+	}
+
+	@RequestMapping("/greetings")
+    public String index() throws Exception {
+        String quote = timer.record(() -> chooseQuote());
+    	distributionSummary.record(quote.length());
     	
-        return quotes.chooseOne();
+		return quote;
     }
+
+	private String chooseQuote() throws InterruptedException {
+    	TimeUnit.MILLISECONDS.sleep(RandomUtils.nextLong(50L, 500L));
+		return quotes.chooseOne();
+	}
 
     @Bean
     public AlwaysSampler defaultSampler() {
